117fdf529f6c10536364da2cd69561956bd59547
ReadingClouds <p.clark@reading.ac.uk>
2026-02-09 11:21:01 +0000

    Change install method

--- setup.py
+++ setup.py
@@ -1,21 +1,6 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    # Needed to silence warnings (and to be a worthwhile package)
-    name='cohobj',
-    url='https://github.com/ReadingClouds/cohobj',
-    author='Peter Clark',
-    author_email='p.clark@reading.ac.uk',
-    # Needed to actually package something
-    packages=['cohobj', 
-              ],
-    # Needed for dependencies
-    install_requires=['numpy', 'scipy', 'scikit-image', 'dask', 'xarray', 'loguru'],
-    # *strongly* suggested for sharing
-    version='0.3.0',
-    # The license can be anything you like
-    license='MIT',
-    description='python code to help identify and use objects in 3D LES data.',
-    # We will also need a readme eventually (there will be a warning)
-    long_description=open('README.md').read(),
+setuptools.setup(
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
 )
